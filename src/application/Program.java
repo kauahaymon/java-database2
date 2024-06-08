5127ff3d656bbec78f17f0c48a61cd694d68b35a
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-07 21:13:14 -0400

    Updating data

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,65 +8,28 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        // Retrieving data from db
-
+        // Updating data from db
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
+            st = conn.prepareStatement("UPDATE seller " +
+                    "SET BaseSalary = BaseSalary + ? " +
+                    "WHERE DepartmentId = ?");
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done ! Rows affected: " + rowsAffected);
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
         finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
-        }
-
-        // Inserting data into db
-
-        PreparedStatement pst = null;
-        try {
-            pst = conn.prepareStatement(
-                    "INSERT INTO seller " +
-                    "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                    "VALUES " +
-                    "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-            pst.setString(1, "Bob Purple");
-            pst.setString(2, "bobpurple@gmail.com");
-            pst.setDate(3, new java.sql.Date(sdf.parse("28/04/1985").getTime()));
-            pst.setDouble(4, 3000.0);
-            pst.setInt(5, 1);
-
-            int rowsAffected = pst.executeUpdate();
-
-            if (rowsAffected > 0) {
-                ResultSet generatedKey = pst.getGeneratedKeys();
-                while (generatedKey.next()) {
-                    int id = generatedKey.getInt(1);
-                    System.out.println("Done! New Id: " + id);
-                }
-            }
-            else {
-                System.out.println("Rows affected: " + rowsAffected);
-            }
-        }
-        catch (SQLException | ParseException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DB.closeStatement(pst);
-            DB.closeConnection();
+            DB.getConnection();
         }
     }
 }
