1e5838c8c3f56e35f390d3d37f19fd6cbfc9c85c
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-07 22:16:12 -0400

    Deleting data

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,23 +9,21 @@ import java.text.SimpleDateFormat;
 public class Program {
     public static void main(String[] args) {
 
-        // Updating data from db
+        // Deleting data from db
         Connection conn = null;
         PreparedStatement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement("UPDATE seller " +
-                    "SET BaseSalary = BaseSalary + ? " +
-                    "WHERE DepartmentId = ?");
-            st.setDouble(1, 200);
-            st.setInt(2, 2);
+            st = conn.prepareStatement("DELETE FROM department " +
+                    "WHERE id = ? ");
+            st.setInt(1, 2);
 
             int rowsAffected = st.executeUpdate();
             System.out.println("Done ! Rows affected: " + rowsAffected);
         }
         catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
         }
         finally {
             DB.closeStatement(st);
